3150ee774f24590c5fa16c4802b8b5b2f900e15a
Guillaume Charpentier <guillaume.prodancer@gmail.com>
2024-09-21 11:07:54 +0200

    Fix errors in App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
-import React, { useState, lazy } from "react";
+import React, { useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { calculator, home, error, todolist } from "./routes";
 import "./styles/index.scss";
 import Preloader from "./components/Preloader/Preloader";
-
-const Calculator = lazy(() => import("./pages/Calculator"));
-const Todolist = lazy(() => import("./pages/Todolist"));
-const Error = lazy(() => import("./pages/Error"));
-const Home = lazy(() => import("./pages/Home"));
+import Home from "./pages/Home";
+import Calculator from "./pages/Calculator";
+import Todolist from "./pages/Todolist";
+import Error from "./pages/Error";
 
 const App = () => {
   const [loading, setLoading] = useState(true);
